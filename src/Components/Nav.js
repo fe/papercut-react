88d21caf92fe26a77f0eb2c8c4ebb8d3c66d50df
Furkan Erkul <dev.furkanerkul@hotmail.com>
2022-11-05 02:27:00 +0300

    add submit handler

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 
 function Nav() {
     const [staticEbat, setStaticEbat] = useState([
@@ -30,16 +30,16 @@ function Nav() {
 
     const [dynamic, setDynamic] = useState([]);
     const [paper, setPaper] = useState({
-        en: 0,
-        boy: 0
+        w: 0,
+        h: 0
     });
 
     const dynamicSelectHandler = (e) => {
         setDynamic([...e.target.selectedOptions].map((item) => {
             const split = item.value.split('x');
             return {
-                en: parseFloat(split[0]),
-                boy: parseFloat(split[1])
+                w: parseFloat(split[0]),
+                h: parseFloat(split[1])
             }
         }));
     }
@@ -63,13 +63,14 @@ function Nav() {
         }
     }
 
-    useEffect(() => {
-        console.log(dynamic);
-    }, [dynamic]);
-
-    useEffect(() => {
-        console.log(paper);
-    }, [paper]);
+    const submitHandler = (e) => {
+        e.preventDefault();
+        if ((paper.w !== 0 && paper.h !== 0) && dynamic.length > 0) {
+            console.log(paper, dynamic);
+        } else {
+            alert('Please fill all fields');
+        }
+    }
 
     return (
         <nav className='col-span-3 bg-gray-100'>
@@ -85,12 +86,12 @@ function Nav() {
                 <div className='flex-initial flex flex-col'>
                     <h4 className='text-base font-semibold mb-2'>İş Bilgisi</h4>
                     <div className='flex w-full gap-2 mb-2'>
-                        <input type="number" name="en" className='w-full form-input' onChange={paperHandler} placeholder='en' />
-                        <input type="number" name="boy" className='w-full form-input' onChange={paperHandler} placeholder='boy' />
+                        <input type="number" name="w" className='w-full form-input' onChange={paperHandler} placeholder='en' />
+                        <input type="number" name="h" className='w-full form-input' onChange={paperHandler} placeholder='boy' />
                     </div>
                 </div>
                 <div className='flex-initial flex flex-col'>
-                    <button type='button' className='light-btn'>
+                    <button type='button' onClick={submitHandler} className='light-btn'>
                         Hesapla
                         <svg aria-hidden="true" className="ml-2 -mr-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
                     </button>
